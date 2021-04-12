b706e2b17bfffaf2e4e1b60298317fa107809ce1
HCTatse <38561093+HCTatse@users.noreply.github.com>
2021-04-12 09:05:41 +0200

    Changed fetch block

--- fabric-version1x/channelOperation/peer_channel_join.sh
+++ fabric-version1x/channelOperation/peer_channel_join.sh
@@ -34,7 +34,7 @@ ORDERER_ADDRESS=orderer.unibw.de:7050
 # Code                                                                                                              #
 #####################################################################################################################
 
-docker exec $CONTAINER_NAME peer channel fetch config -c $CHANNEL_ID -o $ORDERER_ADDRESS --tls --cafile /etc/hyperledger/msp/users/admin/tls/tlsca.unibw.de-cert.pem
+docker exec $CONTAINER_NAME peer channel fetch oldest -c $CHANNEL_ID -o $ORDERER_ADDRESS --tls --cafile /etc/hyperledger/msp/users/admin/tls/tlsca.unibw.de-cert.pem
 sleep 10
 echo "Peer joining channel: $CHANNEL_ID/n"
-docker exec $CONTAINER_NAME peer channel join -b "./"$CHANNEL_ID"_config.block" 
+docker exec $CONTAINER_NAME peer channel join -b "./"$CHANNEL_ID"_oldest.block" 
